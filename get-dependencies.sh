ffb2bf7f0489653c519ea40b21581efbd7971dfe
juju4 <juju4@users.noreply.github.com>
2017-01-03 09:06:05 -0500

    sync repositories - cleaning

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,4 +6,6 @@ umask 022
 rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming
+[ ! -e $rolesdir/juju4.falco ] && ln -s ansible-falco $rolesdir/juju4.falco
 
