c2217cf9a19f4491c10461d677ad29d5d262707d
juju4 <juju4@users.noreply.github.com>
2017-01-04 10:39:18 -0500

    fix test in Jenkinsfile+kitchen

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,6 +6,10 @@ umask 022
 rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
-## galaxy naming
-[ ! -e $rolesdir/juju4.falco ] && ln -s ansible-falco $rolesdir/juju4.falco
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.falco ] && ln -s ansible-falco $rolesdir/juju4.falco
+[ ! -e $rolesdir/juju4.falco ] && cp -R $rolesdir/ansible-falco $rolesdir/juju4.falco
+
+## don't stop build on this script return code
+true
 
