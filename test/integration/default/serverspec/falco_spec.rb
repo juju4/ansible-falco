c434d303c28377bf07a765f15c6e1d3f75824ec6
juju4 <juju4@users.noreply.github.com>
2018-12-16 15:21:20 -0500

    serverspec tests: add process and service check

--- test/integration/default/serverspec/falco_spec.rb
+++ test/integration/default/serverspec/falco_spec.rb
@@ -7,4 +7,12 @@ describe package('falco') do
   it { should be_installed }
 end
 
+describe process("falco") do
+  its(:user) { should eq "root" }
+  its(:args) { should include '--pidfile=/var/run/falco.pid' }
+end
 
+describe service('falco') do
+  it { should be_enabled }
+  it { should be_running }
+end
