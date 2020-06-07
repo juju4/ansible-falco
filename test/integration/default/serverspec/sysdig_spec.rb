1cf29adbc9628dec6d4f5458c339998f88ca22d7
juju4 <juju4@users.noreply.github.com>
2020-06-07 19:56:17 -0400

    test/default: remove sysdig package - not needed anymore

--- test/integration/default/serverspec/sysdig_spec.rb
+++ test/integration/default/serverspec/sysdig_spec.rb
@@ -6,12 +6,3 @@ set :backend, :exec
 describe package('sysdig-dkms'), :if => os[:family] == 'debian' || os[:family] == 'ubuntu' do
   it { should be_installed }
 end
-describe package('sysdig'), :if => os[:family] == 'redhat' do
-  it { should be_installed }
-end
-
-## Not valid if inside a container
-#describe file('/dev/sysdig0') do
-#  it { should be_file }
-#end
-
