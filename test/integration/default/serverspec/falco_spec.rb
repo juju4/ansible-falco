d1e033537f3a52e77beb5a2271427dca093fb816
juju4 <juju4@users.noreply.github.com>
2020-06-07 19:56:50 -0400

    test/default: is_container option - set true

--- test/integration/default/serverspec/falco_spec.rb
+++ test/integration/default/serverspec/falco_spec.rb
@@ -3,16 +3,27 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
+is_container = true
+
 describe package('falco') do
   it { should be_installed }
 end
 
-describe process("falco") do
-  its(:user) { should eq "root" }
-  its(:args) { should include '--pidfile=/var/run/falco.pid' }
-end
+if not is_container
 
-describe service('falco') do
-  it { should be_enabled }
-  it { should be_running }
+  describe file('/dev/falco0') do
+    it { should be_file }
+    it { should be_character_device }
+    it { should be_mode 400 }
+  end
+
+  describe process("falco") do
+    its(:user) { should eq "root" }
+    its(:args) { should include '--pidfile=/var/run/falco.pid' }
+  end
+
+  describe service('falco') do
+    it { should be_enabled }
+    it { should be_running }
+  end
 end
