6aec438b116d8b886e57cf8e378d5e867bbcee4b
juju4 <juju4@users.noreply.github.com>
2023-06-17 12:21:22 +0000

    ci(default-k3s-bare): add testing over k3s/helm

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,6 +6,7 @@ umask 022
 rolesdir=$(dirname $0)/..
 
 [ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
+[ ! -d $rolesdir/cyverse_ansible.ansible_k3s ] && git clone https://github.com/juju4/ansible-k3s $rolesdir/cyverse_ansible.ansible_k3s
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.falco ] && ln -s ansible-falco $rolesdir/juju4.falco
 [ ! -e $rolesdir/juju4.falco ] && cp -R $rolesdir/ansible-falco $rolesdir/juju4.falco
