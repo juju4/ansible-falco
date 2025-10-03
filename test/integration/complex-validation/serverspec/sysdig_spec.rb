a5d0a69f01886562b07dbf9d7be23ea9e4f08624
Tyler Christiansen <code@tylerc.me>
2025-10-02 18:49:43 -0700

    test: switch to molecule for tests

--- test/integration/complex-validation/serverspec/sysdig_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'serverspec'
-
-# Required by serverspec
-set :backend, :exec
-
-describe package('sysdig-dkms'), :if => os[:family] == 'debian' || os[:family] == 'ubuntu' do
-  it { should be_installed }
-end
